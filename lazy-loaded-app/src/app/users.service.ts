31dd4d10355dce4a34c16972c765b3b30cacf46b
nkonada <knc@outlook.in>
2020-08-21 22:30:13 +0530

    user detail component

--- lazy-loaded-app/src/app/users.service.ts
+++ lazy-loaded-app/src/app/users.service.ts
@@ -1,17 +1,16 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpResponse } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpResponse } from "@angular/common/http";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class UsersService {
-
-  constructor( private http: HttpClient) {
-
-  }
+  constructor(private http: HttpClient) {}
 
   public getUsers() {
-    return this.http.get('https://reqres.in/api/users?page=2');
+    return this.http.get("https://reqres.in/api/users?page=1");
+  }
+  public getSingleUser(id) {
+    return this.http.get(`https://reqres.in/api/users/${id}`);
   }
 }
